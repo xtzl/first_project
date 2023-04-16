a8ae19e081910bfaf64917fd833d24b9469e69a4
xtzl <1766264415@qq.com>
2023-04-16 11:02:33 +0800

    Initial commit

---
+++ src/Study2.java
@@ -0,0 +1,20 @@
+public class Study2 {
+
+    public static void main(String[] args) {
+        //\t ：一个制表位，实现对齐的功能
+        System.out.println("姓名 \t年龄\t成绩\t性别\t爱好");
+
+        System.out.println("小明 \t16\t89\t男\t打乒乓球");
+// \n ：换行符
+        System.out.println("jack\nsmith\nmary");
+// \\ ：一个\ \\
+        System.out.println("C:\\Windows\\System32\\cmd.exe");
+// \" :一个" System.out.println("老韩说:\"要好好学习 java,有前途\"");
+// \' ：一个' System.out.println("老韩说:\'要好好学习 java,有前途\'");
+// \r :一个回车 System.out.println("韩顺平教育\r 北京");
+// 解读
+// 1. 输出 韩顺平教育
+// 2. \r 表示回车
+        System.out.println("韩顺平教育\r 北京"); // 北京平教育
+    }
+}
---
+++ src/Study3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Study3 {
+    public static void main(String[] args) {
+        Scanner myscanner = new Scanner(System.in);
+        int a=myscanner.nextInt();
+        System.out.println(a);
+    }
+}
